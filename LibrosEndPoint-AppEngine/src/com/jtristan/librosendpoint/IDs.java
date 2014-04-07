61b644cf8ecb097872e23e5e7cba20a0f3973ec5
jose.tristan.martin <jose.tristan.martin@gmail.com>
2014-04-07 14:59:42 +0200

    Eliminados ids

--- LibrosEndPoint-AppEngine/src/com/jtristan/librosendpoint/IDs.java
+++ LibrosEndPoint-AppEngine/src/com/jtristan/librosendpoint/IDs.java
@@ -2,8 +2,8 @@ package com.jtristan.librosendpoint;
 
 public class IDs {
 
-	//public final static String CLIENT_ID = "***REEMPLAZA POR EL CLIENT_ID obtenido en la consola de Google Cloud";
-	public final static String CLIENT_ID = "128678154032-4c4ad69bmu6i8uh0ukq8ed78eu1687sk.apps.googleusercontent.com";
-	public final static String AUDIENCE = "128678154032-i9f4dut797bbh1d41nkf7rf82l20lipl.apps.googleusercontent.com";
+	public final static String CLIENT_ID = "***REEMPLAZA POR EL CLIENT_ID obtenido en la consola de Google Cloud***";
+	public final static String AUDIENCE = "***REEMPLAZA POR EL WEB_ID obtenido en la consola de Google Cloud***";
+	
 	
 }
